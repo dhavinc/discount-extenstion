86eeeb88ab6e5a04f0791531df4f594fd0f3798b
Dhia Eddine <dhiaeddine214@gmail.com>
2020-05-20 03:52:54 +0100

    payload fixed

--- dist/background.js
+++ dist/background.js
@@ -4,33 +4,42 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.cdiscount.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'www.cdiscount.com' },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
   });
 });
-let contentPort
-chrome.runtime.onConnect.addListener(function(portFrom) {
+let contentPort;
+chrome.runtime.onConnect.addListener(function (portFrom) {
   chrome.extension.getBackgroundPage().console.log('connected');
-   if (portFrom.name === 'background-content') {
-      //This is how you add listener to a port.
-      portFrom.onMessage.addListener(function(message) {
-        // get products
-        chrome.extension.getBackgroundPage().console.log('message arrives', message)
-      });
-   }
+  if (portFrom.name === 'background-content') {
+    //This is how you add listener to a port.
+    portFrom.onMessage.addListener(function (message) {
+      // get products
+      chrome.extension
+        .getBackgroundPage()
+        .console.log('message arrives', message);
+    });
+  }
 });
 
-//Send a message to a tab which has your content script injected. 
+//Send a message to a tab which has your content script injected.
 //You should able to use postMessage here as well.
-// to get current tab ID
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var currTab = tabs[0].id;
-  chrome.extension.getBackgroundPage().console.log('currentTab', currTab);
-  chrome.tabs.sendMessage(currTab, {action: 'GET_DIMENSION'});
-});
+chrome.webNavigation.onCompleted.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'GET_DIMENSION' }, function (
+      response
+    ) {
+      // chrome.extension.getBackgroundPage().console.log(response.farewell);
+    });
+  });
+})
